caaee6a50ebcc863a8ea293636306bfb9fb36d70
Vanessa <suede210810@gmail.com>
2024-05-17 21:00:07 +0200

    create modal schema

--- components/GardenerCard.tsx
+++ components/GardenerCard.tsx
@@ -10,8 +10,17 @@ interface Gardener {
   lastname: string;
   caution: boolean;
 }
+
 const GardenerCards = () => {
+  const [openModal, setOpenModal] = useState(false);
+  const [selectedGardener, setSelectedGardener] = useState<Gardener | null>(null);
+
   const [gardeners, setGardeners] = useState<Gardener[]>([]);
+  const handleOpenModal = (gardener: Gardener) => {
+    setSelectedGardener(gardener);
+    setOpenModal(true);
+  };
+
   useEffect(() => {
     async function getGardeners() {
       const { data, error } = await supabase.from("Gardener").select("*");
@@ -38,20 +47,24 @@ const GardenerCards = () => {
     <div className="grid grid-cols-4 gap-4 grid-rows-9 h-full place-items-center">
       {gardeners.map((gardener, index) => (
         <button
-          className="bg-secondary-50 text-primary-50 items-center flex justify-around w-full h-full  rounded-md "
+          onClick={() => handleOpenModal(gardener)}
+          className="bg-secondary-50 text-primary-50 items-center flex justify-around w-full h-full rounded-md"
           key={index}
         >
           {gardener.lastname} {gardener.firstname}
           <div
-            className={`w-3 h-3 rounded-full ${
-              isValid(gardener.caution) ? "bg-green-500" : "bg-red-500"
-            }`}
+            className={`w-3 h-3 rounded-full ${isValid(gardener.caution) ? "bg-green-500" : "bg-red-500"}`}
           ></div>
         </button>
       ))}
+      {openModal && selectedGardener && (
+        <GardenerInformations
+          gardenerInformations={selectedGardener}
+          handleCloseModal={() => setOpenModal(false)}
+        />
+      )}
     </div>
   );
-  <GardenerInformations gardenerInformations={gardeners}/>
 };
 
 export default GardenerCards;
--- components/Modals/GardenerInformations.tsx
+++ components/Modals/GardenerInformations.tsx
@@ -1,21 +1,57 @@
+"use client";
+
 import { Box, Modal } from "@mui/material";
 import React from "react";
 
-const GardenerInformations = ({ gardenerInformations }) => {
-  const [open, setOpen] = React.useState(false);
-  const handleOpen = () => setOpen(true);
-  const handleClose = () => setOpen(false);
+interface GardenerInformationsProps {
+  gardenerInformations?: {
+    id: number;
+    firstname: string;
+    lastname: string;
+    caution: boolean;
+  };
+  handleCloseModal: () => void;
+}
+
+const GardenerInformations: React.FC<GardenerInformationsProps> = ({
+  gardenerInformations,
+  handleCloseModal,
+}) => {
+  const style = {
+    position: "absolute",
+    display: "flex",
+    justifyContent: "center",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+    height: 368,
+    backgroundColor: "#292929",
+    borderRadius: "0.625rem",
+    fontFamily: "Montserrat",
+    boxShadow: 24,
+    padding: 4,
+    width: {
+      xs: "300px",
+      sm: "320px",
+      md: "430px",
+      lg: "600px",
+    },
+  };
 
   return (
     <Modal
-      open={open}
-      onClose={handleClose}
+      open={true}
+      onClose={handleCloseModal}
       aria-labelledby="modal-modal-title"
       aria-describedby="modal-modal-description"
     >
-      <Box>
-        <button type="button">X</button>
-        <p>hello</p>
+      <Box sx={style}>
+        <button type="button" onClick={handleCloseModal}>
+          X
+        </button>
+        <p>
+          {gardenerInformations?.firstname} {gardenerInformations?.lastname}
+        </p>
       </Box>
     </Modal>
   );
